2541053a024da319ac98ddb16156fa7840bf91c6
ChantYuCN <chengde.yu@intel.com>
2025-09-16 10:51:33 +0800

    looks in my eyes
    Signed-off-by: ChantYuCN <chengde.yu@intel.com>

--- install.sh
+++ install.sh
@@ -15,4 +15,5 @@ sudo install dist/mcp2221hp/mcp2221hp  /usr/local/bin/
 echo 'SUBSYSTEM=="usb", ACTION=="add", RUN+="/usr/local/bin/mcp2221hp --mode attach --domain DOMNAME"' | sudo tee  /etc/udev/rules.d/99-tpe-standalne-mcp2221.rules
 echo 'SUBSYSTEM=="usb", ACTION=="remove", RUN+="/usr/local/bin/mcp2221hp --mode detach --domain DOMNAME"' | sudo tee -a /etc/udev/rules.d/99-tpe-standalne-mcp2221.rules
 sudo sed -i "s/DOMNAME/$DOM/g" /etc/udev/rules.d/99-tpe-standalne-mcp2221.rules
+sudo cp -r dist/mcp2221hp/_internal /usr/local/bin/
 sudo udevadm control --reload-rules
